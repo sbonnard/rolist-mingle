8cee0402b9f1f6a8260b1fe5f1b591b3e210fa77
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-04-18 22:57:09 +0200

    pages html created for flow, forgotten password and create-account but not complete at all. I added some script but I would like to generalize it a bit and it does not work so well

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('.js-concept-dice').addEventListener("mouseover", function(){
+    document.querySelector('[data-concept-dice]').classList.toggle('ttl--primary')
+})
+
+console.log(document.querySelectorAll('[data-concept-dice]'));

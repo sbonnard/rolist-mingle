806cc38a7ebc09b8156483dffe8d5806dc9586e4
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-05-12 22:35:42 +0200

    did some refacto in my js again

--- js/script.js
+++ js/script.js
@@ -39,33 +39,26 @@ let selectedRPG = [];
 inputField.addEventListener('keyup', function (event) {
     const inputText = inputField.value.trim();
     if (inputText !== '') {
-        if (event.key === 'Enter') {
-            const clonedTemplate = template.content.cloneNode(true);
-            clonedTemplate.getElementById('favourite-rpg').innerText = inputText;
-            selectedItemsList.appendChild(clonedTemplate);
-            inputField.value = '';
-        } else {
-            suggestions.innerHTML = "";
-            let testList = JDR.filter(rpg => rpg.name.toLowerCase().includes(inputText.toLowerCase())).sort((a, b) => a.name > b.name ? 1 : b.name > a.name ? -1 : 0);
-            testList.forEach(item => {
-                let newItem = document.createElement('p');
-                newItem.classList.add('js-suggestion', 'suggestions__itm');
-                newItem.setAttribute('id', item.id);
-                newItem.addEventListener('click', function () {
-                    selectedRPG.push(item);
-                    template.content.getElementById('favourite-rpg').innerHTML = item.name;
-                    let clone = document.importNode(template.content, true);
-                    clone.querySelector('.button--minus').addEventListener('click', function (event) {
-                        event.target.parentNode.remove()
-                    })
-                    selectedItemsList.appendChild(clone);
-                    newItem.remove();
-                })
-                const textItem = document.createTextNode(item.name);
-                newItem.appendChild(textItem);
-                suggestions.appendChild(newItem);
-            })
-        }
+        suggestions.innerHTML = "";
+        let testList = JDR.filter(rpg => rpg.name.toLowerCase().includes(inputText.toLowerCase())).sort((a, b) => a.name.localeCompare(b.name));
+        testList.forEach(item => {
+            let newItem = document.createElement('p');
+            newItem.classList.add('js-suggestion', 'suggestions__itm');
+            newItem.setAttribute('id', item.id);
+            newItem.addEventListener('click', function () {
+                selectedRPG.push(item);
+                template.content.getElementById('favourite-rpg').innerHTML = item.name;
+                let clone = document.importNode(template.content, true);
+                clone.querySelector('.button--minus').addEventListener('click', function (event) {
+                    event.target.parentNode.remove();
+                });
+                selectedItemsList.appendChild(clone);
+                newItem.remove();
+            });
+            const textItem = document.createTextNode(item.name);
+            newItem.appendChild(textItem);
+            suggestions.appendChild(newItem);
+        });
     } else {
         suggestions.innerHTML = '';
     }

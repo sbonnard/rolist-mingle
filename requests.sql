41fd210b6b15396808f79ec8264db183fbc59fcf
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-06-06 10:10:04 +0200

    Created every requests from CREATE database to TABLES. Need to check it before working on PHP my admin

--- requests.sql
+++ requests.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE rolist_mingle;
+
 CREATE TABLE localisation (
     id_localisation SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     name_localisation VARCHAR(100) NOT NULL,
@@ -50,4 +52,38 @@ CREATE TABLE user (
     PRIMARY KEY (id_user),
     FOREIGN KEY (id_role_type) REFERENCES role_type(id_role_type),
     FOREIGN KEY (id_localisation) REFERENCES localisation(id_localisation)
-)
+);
+
+CREATE TABLE party (
+    id_party SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    number_players SMALLINT UNSIGNED NOT NULL,
+    game_master BOOLEAN DEFAULT false,
+    id_infos SMALLINT UNSIGNED NOT NULL,
+    id_role_type SMALLINT UNSIGNED NOT NULL,
+    id_localisation SMALLINT UNSIGNED NOT NULL,
+    id_user SMALLINT UNSIGNED NOT NULL, -- GAME_MASTER
+    id_universe SMALLINT UNSIGNED NOT NULL,
+    id_user SMALLINT UNSIGNED NOT NULL, -- PARTY_CREATOR
+    PRIMARY KEY (id_party),
+    FOREIGN KEY (id_infos) REFERENCES infos(id_infos),
+    FOREIGN KEY (id_role_type) REFERENCES role_type(id_role_type),
+    FOREIGN KEY (id_localisation) REFERENCES localisation(id_localisation),
+    FOREIGN KEY (id_user) REFERENCES id_user(id_user), -- GAME_MASTER
+    FOREIGN KEY (id_universe) REFERENCES id_universe(id_universe),
+    FOREIGN KEY (id_user) REFERENCES id_user(id_user), -- PARTY_CREATOR
+);
+
+CREATE TABLE larp (
+    id_larp SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    date_start DATE NOT NULL,
+    date_end DATE NOT NULL,
+    id_infos SMALLINT UNSIGNED NOT NULL,
+    id_universe SMALLINT UNSIGNED NOT NULL,
+    id_localisation SMALLINT UNSIGNED NOT NULL,
+    id_larp_type SMALLINT UNSIGNED NOT NULL,
+    PRIMARY KEY (id_larp),
+    FOREIGN KEY (id_infos) REFERENCES infos(id_infos),
+    FOREIGN KEY (id_universe) REFERENCES id_universe(id_universe),
+    FOREIGN KEY (id_localisation) REFERENCES localisation(id_localisation),
+    FOREIGN KEY (id_larp_type) REFERENCES laid_larp_type(id_larp_type)
+);

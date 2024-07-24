986ffb2d96f9dc1c71f063e5a2a22e21e2717d88
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-07-24 16:45:30 +0200

    head

--- app/includes/components/_head.php
+++ app/includes/components/_head.php
@@ -14,7 +14,7 @@ function fetchHead(string $headTitle):string
         '<meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>' . $headTitle . '</title>
-        <link rel="shortcut icon" href="logo/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="icones/favicon.ico" type="image/x-icon">
         <!-- if development -->
         <script type="module" src="http://localhost:5173/@vite/client"></script>
         <script type="module" src="http://localhost:5173/js/script.js"></script>';
